2e02f3c37877a595d11c68df6ef543bc222b344a
bergman97 <lukas.bergman97@gmail.com>
2016-11-02 12:50:00 +0100

    small changes

--- PersonList/Form1.Designer.cs
+++ PersonList/Form1.Designer.cs
@@ -235,9 +235,7 @@
             this.Controls.Add(this.PersonGroupBox);
             this.Cursor = System.Windows.Forms.Cursors.AppStarting;
             this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Opacity = 0.75D;
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "Person list";
