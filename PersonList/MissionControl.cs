c45bba57298dae98705996b614e0fc5a965488c0
bergman97 <lukas.bergman97@gmail.com>
2016-11-03 16:11:41 +0100

    tried adding restrictions on first and last name textboxes

--- PersonList/MissionControl.cs
+++ PersonList/MissionControl.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PersonList
@@ -10,11 +11,21 @@ namespace PersonList
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        private int parsedId1;
+        private int parsedId2;
 
         public bool CheckInput()
         {
             if ((FirstName.Length == 0) || (LastName.Length == 0))
                 return false;
+            else if ((Regex.IsMatch(FirstName, @"^[-+\p{L}\p{N}]+$") == false) || (Regex.IsMatch(LastName, @"^[-+\p{L}\p{N}]+$") == false))
+            {
+                return false; 
+            }
+            else if (int.TryParse(FirstName, out parsedId1) || (int.TryParse(FirstName, out parsedId2)))
+            {
+                return false;
+            }
             return true;
         }
 
